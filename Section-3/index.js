2ae4fa321670a47de4da4030c95da4a76c5fcb86
WouroudElKhaldi <wouroudelkhaldi@gmail.com>
2023-09-29 12:45:05 +0300

    section 3 final version

--- Section-3/index.js
+++ Section-3/index.js
@@ -3,7 +3,7 @@ const isPrime = (number) => {
     if (number < 2) {
         return false ;
     } else {
-        for (i = 2 ; i < number ; i ++){
+        for (let i = 2 ; i < number ; i ++){
             if ( number % i === 0) {
                 return false
             }
@@ -14,7 +14,7 @@ const isPrime = (number) => {
 
 if (!isNaN(input) && input >= 2) {
     console.log(`prime numbers up tp ${input} are :`)
-    for ( i= 2 ; i <= input ; i++) {
+    for (let i= 2 ; i <= input ; i++) {
         if (isPrime(i)){
             console.log(i);
         }
